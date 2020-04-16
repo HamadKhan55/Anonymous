bc62694790948984dc247eee061dde3100accbc8
HamadKhan55 <63805254+HamadKhan55@users.noreply.github.com>
2020-04-16 23:22:00 +0500

    Update Hamad.py

--- Hamad.py
+++ Hamad.py
@@ -56,9 +56,9 @@ logo = """ -----------------------------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92HAMAD khan
+\033[1;96mInstagram \033[1;93m: \033[1;Cutekhan1434
+\033[1;96mFacebook  \033[1;93m: \033[1; https://m.me/itsAnonymous13456
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
@@ -80,9 +80,9 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92mHamad Khan
+\033[1;96mInstagram \033[1;93m: \033[1;92mCutekhan1434
+\033[1;96mFacebook  \033[1;93m: \033[1;92m https://m.me/itsAnonymous13456
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
@@ -103,7 +103,7 @@ while (loop == 'true'):
             os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https:/
 
 def login():
 	os.system('clear')
